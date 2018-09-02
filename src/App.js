d03724ac5bbaf52b1c9bb0a4e79fd0f9d29cd969
Pete Corey <petecorey@gmail.com>
2018-09-02 10:43:42 -0600

    ASCII chord chart

--- src/App.js
+++ src/App.js
@@ -6,13 +6,18 @@ import logo from "./logo.svg";
 class App extends Component {
   render() {
     return (
-      <div style={{ flexFlow: "wrap", display: "flex", flexDirection: "row" }}>
-        <Chord chord={[null, 10, 10, 9, 12, null]} />
+      <div
+        style={{
+          fontSize: "14px"
+        }}
+      >
+        <Chord chord={[null, 10, 10, 9, 12, null]} name="Cmaj7" />
         <Chord chord={[null, 8, 10, 9, 10, null]} />
         <Chord chord={[null, 8, 9, 7, 10, null]} />
         <Chord chord={[null, 8, 8, 6, 9, null]} />
         <Chord chord={[null, 3, 8, 6, 9, null]} />
         <Chord chord={[null, 3, 2, 0, 1, null]} />
+        <Chord chord={[null, 3, 8, 6, 9, null]} />
       </div>
     );
   }
--- src/Chord.js
+++ src/Chord.js
@@ -1,49 +1,32 @@
 import React from "react";
 import _ from "lodash";
+import styled from "styled-components";
 
-const strokeWidth = 0.08;
-const fretScale = 1.2;
-const fingerSize = 0.8;
+const Wire = styled.span`
+  color: #ccc;
+`;
 
-const Fret = ({ fret }) => (
-  <line
-    x1={0}
-    y1={fret * fretScale}
-    x2={5}
-    y2={fret * fretScale}
-    stroke="black"
-    strokeWidth={strokeWidth}
-    strokeLinecap="round"
-  />
-);
+const Finger = styled.span`
+  color: #333;
+  font-weight: bold;
+`;
 
-const String = ({ string, frets }) => (
-  <line
-    x1={string}
-    y1={0}
-    x2={string}
-    y2={(frets - 1) * fretScale}
-    stroke="black"
-    strokeWidth={strokeWidth}
-    strokeLinecap="round"
-  />
-);
+const Open = styled.span`
+  color: #333;
+  font-weight: bold;
+`;
 
-const Finger = ({ fret, string, frets }) => (
-  <rect
-    x={string - fingerSize / 2}
-    y={(fret - fingerSize / 2) * fretScale + strokeWidth}
-    width={fingerSize}
-    height={fingerSize}
-    rx={0.5}
-    ry={0.5}
-    stroke="black"
-    fill="#333"
-    strokeWidth={strokeWidth}
-  />
-);
+const Fret = styled.span`
+  color: #ccc;
+  font-weight: bold;
+`;
 
-export default ({ chord }) => {
+const Name = styled.span`
+  color: #ccc;
+  font-weight: bold;
+`;
+
+export default ({ chord, name }) => {
   let min = _.chain(chord)
     .reject(_.isNull)
     .min()
@@ -52,37 +35,64 @@ export default ({ chord }) => {
     .reject(_.isNull)
     .max()
     .value();
-  let frets = Math.max(6, max - min + 3);
+  let frets = Math.max(5, max - min);
 
   return (
-    <svg
-      viewBox={`-0.5 -0.5 5 ${frets + 1}`}
-      preserveAspectRatio="xMidYMin meet"
-      style={{ border: "1px solid tomato", flexBasis: "25%" }}
+    <pre
+      style={{
+        fontFamily: "Source Code Pro",
+        padding: "2em",
+        display: "inline-block",
+        verticalAlign: "top"
+      }}
     >
       {_.chain(new Array(frets))
         .fill()
-        .map((_, fret) => <Fret key={fret} fret={fret} />)
+        .map((_fret, fret) => fret + min)
+        .map(fret => {
+          return _.flatten([
+            fret == min && min != 0 ? (
+              <Fret>{_.pad(min, 2)}</Fret>
+            ) : (
+              <span>&nbsp;&nbsp;</span>
+            ),
+            _.chain(new Array(6))
+              .fill()
+              .map((_string, string) => {
+                return chord[string] == fret ? (
+                  fret == 0 ? (
+                    <Open>○</Open>
+                  ) : (
+                    <Finger>●</Finger>
+                  )
+                ) : fret == 0 ? (
+                  <Wire>┬</Wire>
+                ) : (
+                  <Wire>│</Wire>
+                );
+              })
+              .concat([
+                fret == min && name ? (
+                  <span>
+                    &nbsp;<Name>{name}</Name>
+                  </span>
+                ) : (
+                  <span>&nbsp;</span>
+                )
+              ])
+              .value()
+          ]);
+        })
+        .map(row => [
+          row,
+          <span>
+            <br />&nbsp;&nbsp;
+            <Wire>├┼┼┼┼┤</Wire>
+            &nbsp;<br />
+          </span>
+        ])
+        .flatten()
         .value()}
-      <text
-        x={-1.25}
-        y={1.5}
-        style={{ fontSize: "1px", fontFamily: "monospace" }}
-      >
-        {min}
-      </text>
-      <String string={0} frets={frets} />
-      <String string={1} frets={frets} />
-      <String string={2} frets={frets} />
-      <String string={3} frets={frets} />
-      <String string={4} frets={frets} />
-      <String string={5} frets={frets} />
-      {_.map(
-        chord,
-        (fret, string) =>
-          fret != null ? <Finger fret={fret - min + 1} string={string} /> : null
-      )}
-      {/* <Finger fret={2} string_from={4} string_to={5} /> */}
-    </svg>
+    </pre>
   );
 };
