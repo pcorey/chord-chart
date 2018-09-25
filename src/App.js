d01c79c434d22273a8b77e82f3f8a24f472adf92
Pete Corey <petecorey@gmail.com>
2018-09-25 16:54:08 -0600

    react-dat-gui

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
 import "./App.css";
 import Chord from "./Chord";
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import styled from "styled-components";
 
-class App extends Component {
-  render() {
-    return (
-      <div
-        style={{
-          fontSize: "14px"
-        }}
-      >
-        <Chord chord={[null, 10, 10, 9, 12, null]} name="Cmaj7" />
-        <Chord chord={[null, 8, 10, 9, 10, null]} />
-        <Chord chord={[null, 8, 9, 7, 10, null]} />
-        <Chord chord={[null, 8, 8, 6, 9, null]} />
-        <Chord chord={[null, 3, 8, 6, 9, null]} />
-        <Chord chord={[null, 3, 2, 0, 1, null]} />
-        <Chord chord={[null, 3, 8, 6, 9, null]} />
-        <Chord
-          chord={[null, [10, 2], [10, 3], [9, 1], [12, 4], null]}
-          name="Cmaj7"
-        />
-      </div>
-    );
-  }
-}
+const App = styled.div`
+  font-size: 18px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
-export default App;
+export default () => (
+  <App>
+    <Chord chord={[null, 10, 10, 9, 12, null]} />
+    <Chord chord={[null, 8, 10, 9, 10, null]} />
+    <Chord chord={[null, 8, 9, 7, 10, null]} />
+    <Chord chord={[null, 8, 8, 6, 9, null]} />
+    <Chord chord={[null, 3, 8, 6, 9, null]} />
+    <Chord chord={[null, 3, 2, 0, 1, null]} />
+    <Chord chord={[null, 3, 8, 6, 9, null]} />
+    <Chord
+      chord={[null, [10, 2], [10, 3], [9, 1], [12, 4], null]}
+      name="Cmaj7"
+    />
+  </App>
+);
--- src/Chord.js
+++ src/Chord.js
@@ -1,13 +1,40 @@
 import React from "react";
 import _ from "lodash";
 import styled from "styled-components";
+import DatGui, {
+  DatBoolean,
+  DatButton,
+  DatColor,
+  DatFolder,
+  DatNumber,
+  DatSelect,
+  DatString
+} from "react-dat-gui";
+import "./react-dat-gui.css";
+
+const Chord = styled.pre`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding: 0;
+  margin: 1em 2em;
+  vertical-align: top;
+  border: 0 dashed black;
+`;
+
+const Chart = styled.pre`
+  font-family: "Source Code Pro";
+  text-align: center;
+  padding-right: 4em;
+`;
 
 const Wire = styled.span`
-  color: #ccc;
+  color: #333;
 `;
 
 const Finger = styled.span`
-  color: #333;
+  color: #ccc;
   font-weight: bold;
 `;
 
@@ -16,7 +43,7 @@ const Fingering = styled.span`
 `;
 
 const Open = styled.span`
-  color: #333;
+  color: #ccc;
   font-weight: bold;
 `;
 
@@ -26,6 +53,10 @@ const Fret = styled.span`
 
 const Name = styled.span`
   color: #ccc;
+`;
+
+const Controls = styled.div`
+  width: 100%;
 `;
 
 export default ({ chord, name }) => {
@@ -42,71 +73,132 @@ export default ({ chord, name }) => {
   let frets = Math.max(5, max - min);
 
   return (
-    <pre
-      style={{
-        fontFamily: "Source Code Pro",
-        padding: "2em",
-        display: "inline-block",
-        verticalAlign: "top"
-      }}
-    >
-      {_.chain(new Array(frets))
-        .fill()
-        .map((_fret, fret) => fret + min)
-        .map(fret => {
-          return _.flatten([
-            fret == min && min != 0 ? (
-              <Fret>{_.pad(min, 2)}</Fret>
-            ) : (
-              <span>&nbsp;&nbsp;</span>
-            ),
-            _.chain(new Array(6))
-              .fill()
-              .map((_string, string) => {
-                return (_.isArray(chord[string])
-                  ? chord[string][0]
-                  : chord[string]) == fret ? (
-                  fret == 0 ? (
-                    <Open>○</Open>
+    <Chord>
+      <Chart>
+        {_.chain(new Array(frets))
+          .fill()
+          .map((_fret, fret) => fret + min)
+          .map(fret => {
+            return _.flatten([
+              fret == min && min != 0 ? (
+                <Fret>{_.pad(min, 2)}</Fret>
+              ) : (
+                <span>&nbsp;&nbsp;</span>
+              ),
+              _.chain(new Array(6))
+                .fill()
+                .map((_string, string) => {
+                  return (_.isArray(chord[string])
+                    ? chord[string][0]
+                    : chord[string]) == fret ? (
+                    fret == 0 ? (
+                      <Open>○</Open>
+                    ) : (
+                      <Finger>●</Finger>
+                    )
+                  ) : fret == 0 ? (
+                    <Wire>┬</Wire>
                   ) : (
-                    <Finger>●</Finger>
+                    <Wire>│</Wire>
+                  );
+                })
+                .concat([
+                  fret == min && name ? (
+                    <span>
+                      &nbsp;<Name>{name}</Name>
+                    </span>
+                  ) : name ? (
+                    <span>{_.repeat(" ", name.length + 1)}</span>
+                  ) : (
+                    <span>&nbsp;</span>
                   )
-                ) : fret == 0 ? (
-                  <Wire>┬</Wire>
-                ) : (
-                  <Wire>│</Wire>
-                );
-              })
-              .concat([
-                fret == min && name ? (
-                  <span>
-                    &nbsp;<Name>{name}</Name>
-                  </span>
-                ) : (
-                  <span>&nbsp;</span>
-                )
-              ])
-              .value()
-          ]);
-        })
-        .map(row => [
-          row,
-          <span>
-            <br />&nbsp;&nbsp;
-            <Wire>├┼┼┼┼┤</Wire>
-            &nbsp;<br />
-          </span>
-        ])
-        .flatten()
-        .concat(
-          "  ",
-          _.chain(chord)
-            .map(string => (
-              <Fingering>{_.isArray(string) ? string[1] : " "}</Fingering>
-            ))
-            .value()
-        )
-        .value()}
-    </pre>
+                ])
+                .value()
+            ]);
+          })
+          .map(row => [
+            row,
+            <span>
+              <br />&nbsp;&nbsp;
+              <Wire>├┼┼┼┼┤</Wire>
+              {name ? _.repeat(" ", name.length + 1) : " "}
+              <br />
+            </span>
+          ])
+          .flatten()
+          .concat(
+            "  ",
+            _.chain(chord)
+              .map(string => (
+                <Fingering>{_.isArray(string) ? string[1] : " "}</Fingering>
+              ))
+              .value(),
+            name ? _.repeat(" ", name.length + 1) : " "
+          )
+          .value()}
+      </Chart>
+      <Controls>
+        <DatGui
+          data={{
+            root: "C",
+            quality: "Major 7",
+            voice: Math.random(),
+            finger: Math.random(),
+            gravity: Math.random(),
+            isAwesome: true,
+            feelsLike: "#CCCCCC"
+          }}
+          onUpdate={() => {}}
+        >
+          <DatSelect
+            path="root"
+            label="Root"
+            options={[
+              "C",
+              "C#/Db",
+              "D",
+              "D#/Eb",
+              "E",
+              "F",
+              "F#/Gb",
+              "G",
+              "G#/Ab",
+              "A",
+              "A#/Bb",
+              "B"
+            ]}
+          />
+          <DatSelect
+            path="quality"
+            label="Quality"
+            options={["Major", "Minor", "Major 7", "Minor 7"]}
+          />
+          <br />
+          <DatBoolean path="isAwesome" label="Linked?" />
+          <br />
+          <DatNumber
+            path="voice"
+            label="Voice leading"
+            min={0}
+            max={1}
+            step={0.01}
+          />
+          <DatNumber
+            path="finger"
+            label="Playability"
+            min={0}
+            max={1}
+            step={0.01}
+          />
+          <DatNumber
+            path="gravity"
+            label="Invertedness"
+            min={0}
+            max={1}
+            step={0.01}
+          />
+        </DatGui>
+      </Controls>
+    </Chord>
   );
 };
